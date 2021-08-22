ae3ff3c0a2fb57f9c9126bbe04cfb5493dded820
Erik Vasconcellos <erikvasconcellos@gmail.com>
2021-08-21 23:03:42 -0300

    Cambios para prueba por Entrega 5 a corregir.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
-const productos = [
+/* const productos = [
   "",
   "Kit de cristales chakra",
   "Portavelas",
   "Sahumerio",
   "Sahumador de barro",
 ];
-const precios = [0, 400, 250, 300, 450];
+const precios = [0, 400, 250, 300, 450]; */
+
+
+
+class Productos {
+  constructor (nombre, precio, stock) {
+    this.nombre = nombre || undefined
+    this.precio = precio || undefined
+    this.stock = stock || undefined
+    
+  }
+}
+
+
+  const Portavelas = new Productos ("Portavelas", 250, 100);
+  const kitCristales = new Productos ("Kit cristales 7 chakras", 600, 100);
+  const Sahumador = new Productos ("Sahumador de barro", 400, 100);
+  const Sahumerios = new Productos ("Pack de 10 sahumerios", 350, 100);
+
+
 
 const articulos = prompt(
-  `Hola, selecciona el número del artículo que quieres comprar: \n\n1) Kit de cristales chakra\n\n2) Portavelas \n\n3) Kit de sahumerios \n\n4) Sahumador de barro`
+  `Hola, selecciona el número del artículo que quieres comprar: \n\n1) ${kitCristales.nombre} \n\n2) ${Portavelas.nombre} \n\n3) ${Sahumerios.nombre} \n\n4) ${Sahumador.nombre}`
 );
 
-let stock = 100;
+/* let stock = 100; */
 let cantidad = prompt("Elija la cantidad de artículos que quiere comprar");
 
 function sumaTotal() {
-  if (cantidad <= stock && cantidad >= 1) {
-    const totalComprado = precios[articulos] * cantidad;
+  if (cantidad <= Productos.stock && cantidad >= 1) {
+    const totalComprado = Productos.precio[articulos] * cantidad;
     return totalComprado;
   } else {
     alert("No disponemos de stock suficiente, pruebe con otra cantidad.");
@@ -38,9 +57,12 @@ function montoFinal() {
 
 function total() {
   document.write(
-    `<h3>Felicidades, compraste un ${
-      productos[articulos]
-    }. <br>Su total es de: $${montoFinal()}.</b></h3> `
+    `<h3>Felicidades, compraste un 
+    ${articulos[Productos.precio]}. <br>
+      Su total es de: $${montoFinal()}.</b></h3> `
   );
 }
 total();
+
+
+
