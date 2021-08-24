ca46cc927c54382669d416fa9f325c0f0e8de180
Erik Vasconcellos <erikvasconcellos@gmail.com>
2021-08-24 01:04:59 -0300

    Entrega nro 5

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
+class Productos {
+  constructor(nombre, precio, stock) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
+}
+
 const productos = [
   "",
-  "Kit de cristales chakra",
-  "Portavelas",
-  "Sahumerio",
-  "Sahumador de barro",
+  new Productos(`Kit cristales 7 chakras`, 250, 100),
+  new Productos(`Portavelas`, 600, 120),
+  new Productos(`Sahumador de barro`, 400, 130),
+  new Productos(`Pack de 10 sahumerios`, 350, 80),
 ];
-const stock = [0, 100, 120, 130, 80];
-const precios = [0, 400, 250, 300, 450];
 
 const articulos = prompt(
-  `Hola, selecciona el número del artículo que quieres comprar: \n\n1) Kit de cristales chakra\n\n2) Portavelas \n\n3) Kit de sahumerios \n\n4) Sahumador de barro`
+  `Hola, selecciona el número del artículo que quieres comprar: \n\n1) ${productos[1].nombre}\n\n2) ${productos[2].nombre} \n\n3) ${productos[3].nombre} \n\n4) ${productos[4].nombre}`
 );
 
 let cantidad = prompt("Elija la cantidad de artículos que quiere comprar");
 
 function sumaTotal() {
-  if (cantidad <= stock[articulos] && cantidad >= 1) {
-    const totalComprado = precios[articulos] * cantidad;
+  if (cantidad <= productos[articulos].stock && cantidad >= 1) {
+    const totalComprado = productos[articulos].precio * cantidad;
     return totalComprado;
   } else {
     alert("No disponemos de stock suficiente, pruebe con otra cantidad.");
   }
 }
+
 function montoFinal() {
   let financiacion = prompt(
     `El monto a pagar es: ${sumaTotal()} , elija su método de pago: \n\n1. Efectivo = Accedes a un 5% de descuento sobre el total. \n2. Tarjeta de crédito o débito = ${sumaTotal()}.`
@@ -38,7 +45,7 @@ function montoFinal() {
 function total() {
   document.write(
     `<h3>Felicidades, compraste un ${
-      productos[articulos]
+      productos[articulos].nombre
     }. <br>Su total es de: $${montoFinal()}.</b></h3> `
   );
 }
